e0e98552239cbee4412185ad88cc89f13e3e01d4
GeorgieLiefman <gkb.liefman@gmail.com>
2021-10-28 14:39:36 +1100

    Added dummy endpoints to routes

--- q_and_a_app/__init__.py
+++ q_and_a_app/__init__.py
@@ -2,24 +2,69 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+
+# Homepage
+@app.route('/home/', methods=["PUT", "PATCH"])
+def home_page():
     return "This page will be a homepage for users to signup and login."
 
-@app.route('/feed/')
-def get_students():
-    return "This page will display users' feed."
+    
+# Feed page
+@app.route('/feed/', methods=["GET"])
+def feed():
+    return "This page will display users' feed with latest answered questions."
 
-@app.route('/profile/<int:username>/')
-def get_students(username):
+
+# Users' profile page
+@app.route('/profile/<string:username>/', methods=["GET"])
+def profile(username):
     return f"This page will display {username}'s' profile and their previously asked questions."
 
-@app.route('/unanswered_question/')
-def get_students():
+@app.route('/profile/<string:username>/', methods=["Delete"])
+def profile(username):
+    return f"This page will display {username}'s' profile and let them delete their previously asked questions."
+
+@app.route('/profile/<string:username>/', methods=["PUT", "PATCH"])
+def profile(username):
+    return f"This page will display {username}'s' profile and let them edit their previously asked questions."
+
+
+
+# Edit profile page
+@app.route('/profile/<string:username>/edit', methods=["PUT", "PATCH"])
+def profile(username):
+    return f"This page will display {username}'s' profile and let them edit their profile."
+
+
+
+# Unanswered questions page
+@app.route('/question/unanswered/', methods=["GET"])
+def unanwsered_qs():
     return "This page will display users' unanswered questions."
 
-@app.route('/ask_a_question/')
-def get_students():
+@app.route('/question/unanswered/', methods=["POST"])
+def unanwsered_qs():
+    return "This page will allow for users to answer unanswered questions."
+
+
+# Previously answered questions page
+@app.route('/question/answered/', methods=["GET"])
+def unanwsered_qs():
+    return "This page will display any questions the current user has answered."
+
+@app.route('/question/answered/', methods=["PUT", "PATCH"])
+def unanwsered_qs():
+    return "This will allow for users to edit their previous answers."
+
+@app.route('/question/answered/', methods=["DELETE"])
+def unanwsered_qs():
+    return "This will allow for users to delete their previous answers."
+
+
+
+# Submit question page
+@app.route('/question/submit/', methods=["POST"])
+def ask_a_qs():
     return "This page will allow users to submit a question to the site."
 
 if __name__ == '__main__':
