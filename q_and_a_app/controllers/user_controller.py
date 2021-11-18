f6f6a515ccbb3d63663d415a6f285b4a9ada582b
GeorgieLiefman <gkb.liefman@gmail.com>
2021-11-18 23:29:49 +1100

    Added views for user index and signup page, app in working state

--- q_and_a_app/controllers/user_controller.py
+++ q_and_a_app/controllers/user_controller.py
@@ -7,7 +7,7 @@ from marshmallow import ValidationError
 
 @lm.user_loader
 def load_user(user):
-    return User.query(user)
+    return User.query.get(user)
 
 @lm.unauthorized_handler
 def unauthorized():
@@ -16,10 +16,24 @@ def unauthorized():
 users = Blueprint("users", __name__)
 
 # Displays index of all users
+# The GET routes endpoint
 @users.route("/users/", methods=["GET"])
-def get_user():
+def get_users():
     data = {
-        "page_title": "User Index",
-        "users": users_schema.dump(User.query.all())
+    "page_title": "User Index",
+    "users": users_schema.dump(User.query.all())
     }
-    return render_template("user_index.html", page_data=data)
+    return render_template("user_index.html", page_data = data)
+
+@users.route("/users/signup/", methods = ["GET", "POST"])
+def sign_up():
+    data = {"page_title": "Sign Up"}
+    
+    if request.method == "GET":
+        return render_template("signup.html", page_data = data)
+    
+    new_user = user_schema.load(request.form)
+    db.session.add(new_user)
+    db.session.commit()
+    login_user(new_user)
+    return redirect(url_for("users.get_users"))
