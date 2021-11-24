360e7809e8709acd95b47fbcdc74b7be7d88e785
GeorgieLiefman <gkb.liefman@gmail.com>
2021-11-24 22:57:30 +1100

    fixed features caused by incorrect wording on models

--- q_and_a_app/models/posts.py
+++ q_and_a_app/models/posts.py
@@ -5,6 +5,6 @@ class Post(db.Model):
         post_id = db.Column(db.Integer, primary_key=True)
         post_title = db.Column(db.String(80), unique=True, nullable=False)
         post_content = db.Column(db.String(500), server_default="No description provided...")
-        likes = db.Column(db.Integer, nullable=False, default=0)
+        likes = db.Column(db.Integer, nullable=False, server_default="0")
 
         
