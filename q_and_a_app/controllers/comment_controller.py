a5a4cb2192f700e612933fd7464c2e42dfd72cf8
GeorgieLiefman <gkb.liefman@gmail.com>
2021-12-16 21:45:43 +1100

    Post function for comments working

--- q_and_a_app/controllers/comment_controller.py
+++ q_and_a_app/controllers/comment_controller.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for, current_app, abort, flash
 from main import db
 from models.posts import Post
-from schemas.post_schema import post_schema, posts_schema
 from models.comments import Comment
 from schemas.comment_schema import comment_schema, comments_schema
 import boto3
@@ -21,7 +20,8 @@ def create_comment(id):
         post = Post.query.get_or_404(id)
         if post:
             comment = Comment(text = text, commentor_id = current_user.id, post_id = id)
-            db.session.add(comment)
+            new_comment = comment_schema.load(request.form)
+            db.session.add(new_comment)
             db.session.commit()
 
         else:
